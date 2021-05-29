ec6b4ceafabebf58f4a69df2582b2b548e509163
Kimsol <ksol92789@gmail.com>
2021-05-29 19:33:34 +0900

    유니티 파일 업로드
    키인풋, 캐릭터 이동,점프 구현

---
+++ SungMin_Study/Assets/03.Scripts/Player/PlayerInput.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    private Vector2 moveVector; // 이동 관련 벡터
+    public Vector2 p_moveVector { get { return moveVector; } }
+
+    private bool isJumping = false;  // 점프중인지 판별
+    public bool p_isJumping { get { return isJumping; } set { isJumping = value; } }
+
+    private bool isAttack = false;  // 공격중인지 판별
+    public bool p_isAttack { get { return isAttack; } set { isAttack = value; } }
+
+    private bool isFireBallAttack = false;
+    public bool p_isFireBallAttack { get { return isFireBallAttack; } set { isFireBallAttack = value; } }
+
+
+    private void Update() // 프레임마다 입력값 확인
+    {
+        GetMoveInput(); 
+        GetJumpInput(); 
+        GetAttackInput();
+    }
+
+    private void GetMoveInput() // 좌우 이동
+    {
+        Vector2 moveInput;
+        moveInput.x = Input.GetAxis("Horizontal"); 
+        moveVector = moveInput.x * Vector2.right; 
+    }
+
+    private void GetJumpInput() // 점프
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            if ( isJumping == false) // 점프중이 아닐때만
+                isJumping = true; // 점프 true로 설정.
+        }
+    }
+
+    private void GetAttackInput() // 공격
+    {
+        if (Input.GetKey(KeyCode.Q))
+        {
+            if (isAttack == false)
+                isAttack = true; // 공격 true로 설정.
+        }
+
+        if (Input.GetKey(KeyCode.W)) //파이어볼 발사
+        {
+            if(isFireBallAttack == false)
+            {
+                isFireBallAttack = true;
+            }
+
+        }
+
+    }
+}
---
+++ SungMin_Study/Assets/03.Scripts/Player/PlayerMove.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    private Rigidbody2D rigid;
+    private PlayerInput playerInput; // Input Manager
+
+    private float moveSpeed = 4.0f; // 이동 속도
+    private float JumpPower = 6.0f; // 점프 파워
+    private int JumpCount = 0; // 점프 몇번했는지 카운트
+
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        playerInput = GetComponent<PlayerInput>();
+    }
+
+    private void FixedUpdate()
+    {
+        Move(); // 이동
+        Jump(); // 점프
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Floor")) // 땅에 닿으면 점프 카운트 0으로 초기화
+        {
+            playerInput.p_isJumping = false;
+            JumpCount = 0;
+        }
+    }
+
+    private void Jump()
+    {
+        if (playerInput.p_isJumping == true && JumpCount < 1) // 점프카운트가 0이고 인풋값 받았을때
+        {
+            rigid.AddForce(Vector2.up * JumpPower, ForceMode2D.Impulse); // 플레이어 점프!
+            JumpCount++; // 카운트 올려주기 ( 다시 점프 못하게 )
+        }
+        else return; // 점프할 조건이 안된다면 함수 끝!
+    }
+
+    private void Move()
+    {
+        if(playerInput.p_isAttack == false)
+        {
+            if (playerInput.p_moveVector.x > 0) // 좌우반전
+                this.transform.localScale = new Vector3(1, 1, 1);
+            else if (playerInput.p_moveVector.x < 0)
+                this.transform.localScale = new Vector3(-1, 1, 1);
+            this.transform.Translate(playerInput.p_moveVector * moveSpeed * Time.deltaTime); // 플레이어 이동
+        }      
+    }
+}
