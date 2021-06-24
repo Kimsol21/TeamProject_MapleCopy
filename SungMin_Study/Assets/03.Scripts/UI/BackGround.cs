d001d81003411f1b0d78a8df51a9fbe1fe96115b
CherryB2 <tgbnm7@naver.com>
2021-06-24 16:58:31 +0900

    씬3개&BGM
    시작, 성공, 실패 씬 + BGM추가

---
+++ SungMin_Study/Assets/03.Scripts/UI/BackGround.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGround : MonoBehaviour
+{
+    private bool IsBack;
+
+    void Start()
+    {
+        IsBack = false;
+    }
+    void Update()
+    {
+        BackGroundMove();
+    }
+
+    private void BackGroundMove()
+    {
+        gameObject.transform.Translate(Vector3.left * Time.deltaTime);
+        if(gameObject.transform.position.x < -4& !IsBack)
+        {
+            BackGroundCreate();
+            Destroy(gameObject, 15.0f);
+            IsBack = true;
+        }
+        
+    }
+
+    private void BackGroundCreate()
+    {
+        Instantiate(gameObject, new Vector3(13, 0, 0), Quaternion.identity);
+        IsBack = false;
+    }
+
+}
---
+++ SungMin_Study/Assets/03.Scripts/UI/ButtonUI.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class ButtonUI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GoGameScene()
+    {
+       SceneManager.LoadScene(1);
+    }
+
+    public void GoMainScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void GoSuccessScene()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void GoFailScene()
+    {
+        SceneManager.LoadScene(3);
+    }
+}
